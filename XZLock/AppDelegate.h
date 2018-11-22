82d5ca35213e9adc8a89e99cd808141e6e595593
kkxz <kkxz79@126.com>
2018-11-22 10:57:46 +0800

    Initial Commit

---
+++ XZLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZLock
+//
+//  Created by kkxz on 2018/11/22.
+//  Copyright © 2018 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZLock
+//
+//  Created by kkxz on 2018/11/22.
+//  Copyright © 2018 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
